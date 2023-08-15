a2ae14e9e7fdfdfad3e56e7d4ea5162d0f8ed0b4
Zig Hooker <zighooker@gmail.com>
2023-08-15 03:37:35 -0700

    Build basis of score tracking over rounds

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Create necessary variables
 let computerSelection;
 let playerSelection;
-let roundResult;
+let computerScore = 0;
+let playerScore = 0;
 
 // getComputerChoice randomly determines strings rock, paper, or scissor
 function getComputerChoice() {
@@ -16,17 +17,16 @@ function getComputerChoice() {
         case 2:
             computerSelection = 'paper';
             break;
-        default:
-            console.log("Computer selection error");
     }
+    console.log("getComputerChoice " + computerSelection);
 }
 
 // getPlayerChoice prompts user, equalizes output and checks for validity
 function getPlayerChoice() {
     playerSelection = (prompt("Rock, paper, or scissor?")).toLowerCase();
-    if (playerSelection != 'rock'
-        || playerSelection != 'paper'
-        || playerSelection != 'scissor') {
+    if (playerSelection !== 'rock'
+        && playerSelection !== 'paper'
+        && playerSelection !== 'scissor') {
             alert("Invalid input, please try again.");
             getPlayerChoice();
     }
@@ -35,30 +35,54 @@ function getPlayerChoice() {
 // playRound compares computerSelection and playerSelection to determine winner
 function playRound(playerSelection, computerSelection) {
     if (computerSelection === playerSelection) {
-        roundResult = 'draw';
+        console.log("Draw!");
+        // getComputerChoice();
+        // getPlayerChoice();
+        // playRound(playerSelection, computerSelection);
     } else if (playerSelection === 'rock') {
         if (computerSelection === 'scissor') {
-            roundResult = 'player win';            
+            playerWin();            
         } else {
-            roundResult = 'computer win';
+            computerWin();
         }
     } else if (playerSelection === 'scissor') {
         if (computerSelection === 'paper') {
-            roundResult = 'player win';
+            playerWin();
         } else {
-            roundResult = 'computer win';
+            computerWin();
         }
     } else {
         if (computerSelection === 'rock') {
-            roundResult = 'player win';
+            playerWin();
         } else {
-            roundResult = 'computer win';
+            computerWin();
         }
     }
+}
 
+function playerWin() {
+    console.log("Player wins");
+    playerScore++;
+}
+
+function computerWin() {
+    console.log("Computer wins");
+    computerScore++;
 }
 
 // game runs a sequence of rounds and keeps score
 function game() {
-
+    // if (computerScore < 5 && playerScore < 5) {
+    //     getComputerChoice();
+    //     getPlayerChoice();
+    //     playRound(playerSelection, computerSelection);
+    // } else if (computerScore === 5) {
+    //     console.log("Computer wins the game!");
+    // } else {
+    //     console.log("Player wins the game!");
+    // }
 }
+
+getComputerChoice();
+getPlayerChoice();
+playRound(playerSelection, computerSelection);
